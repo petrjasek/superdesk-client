1ef06cfc2e53c82169b4fdf50c1612557af8e8f6
petr.jasek <petr.jasek@sourcefabric.org>
2014-04-10 17:11:51 +0200

    feat(server): add postinstall script
    adds script for bower install and grunt build

--- web.js
+++ web.js
@@ -1,11 +1,19 @@
+
+'use strict';
+
 var express = require('express');
 var logfmt = require('logfmt');
 var app = express();
 
 app.use(logfmt.requestLogger());
+app.use(express['static'](__dirname + '/dist'));
 
 app.get('/api', function(req, res) {
-    res.send('API here');
+    res.json({
+        _links: [
+            {rel: 'users', href: '/api/users/'}
+        ]
+    });
 });
 
 var port = Number(process.env.PORT || 5000);
