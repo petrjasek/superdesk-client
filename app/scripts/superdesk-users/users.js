a6565475f01ed7032be755ff7d0225b5ddf23da0
Nikola Nikolic <nikolic_nikola@live.com>
2014-11-24 10:49:28 +1100

    fix(roles): Show role in users table
    Show error in users list if there is no role defined
    Fix error in creating roles

--- app/scripts/superdesk-users/users.js
+++ app/scripts/superdesk-users/users.js
@@ -165,6 +165,14 @@
         $scope.selected = {user: null};
         $scope.createdUsers = [];
 
+        api('roles').query().then(function(result) {
+            $scope.roles = _.indexBy(result._items, '_id');
+        });
+
+        $scope.isEmpty = function(object) {
+            return _.isEmpty(object);
+        };
+
         $scope.preview = function(user) {
             $scope.selected.user = user;
         };
@@ -956,6 +964,7 @@
             return {
                 templateUrl: asset.templateUrl('superdesk-users/views/user-list-item.html'),
                 scope: {
+                    roles: '=',
                     users: '=',
                     selected: '=',
                     done: '='
